73b713a1ee8a6d6d148f16dd7bb1725ae9d57dfb
Diksha217-ai <202401080032@mitaoe.ac.in>
2025-06-18 10:49:02 +0530

    Create relationalDemo.java

---
+++ relationalDemo.java
@@ -0,0 +1,13 @@
+public class relationalDemo{
+public static void main(String[] args) {
+        System.out.println("\n--- Relational Operators ---");
+        int a = 100;
+        int b = 20;
+        
+        if (a < b) {
+            System.out.println("a is less than b");
+        } else {
+            System.out.println("a is greater than b");
+        }
+    }
+}
