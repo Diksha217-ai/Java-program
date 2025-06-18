d787c148a9ed710bad538150ec919338ad95d890
Diksha217-ai <202401080032@mitaoe.ac.in>
2025-06-18 10:39:45 +0530

    Create incrementDemo.java

---
+++ incrementDemo.java
@@ -0,0 +1,11 @@
+public class incrementDemo{
+public static void main(String[] args) {
+        System.out.println("\n--- Increment/Decrement ---");
+        int i = 5;
+        i++;
+        i++;
+        i += 6; // 13
+        i--;
+        System.out.println("The value of i after incrementing is: " + i);
+    }
+}
