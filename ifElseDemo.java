df873e68b1c38fa5a44f08a9ed5020f74d91d10c
Diksha217-ai <202401080032@mitaoe.ac.in>
2025-06-18 10:45:23 +0530

    Create ifElseDemo.java

---
+++ ifElseDemo.java
@@ -0,0 +1,12 @@
+public class ifElseDemo{
+public static void main(String[] args) {
+        System.out.println("\n--- If-Else ---");
+        int a = 45;
+        // to check a is even or odd
+        if (a % 2 == 0) {
+            System.out.println("a is even");
+        } else {
+            System.out.println("a is odd");
+        }
+    }
+}
