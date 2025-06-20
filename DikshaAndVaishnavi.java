9fb54b6819379559111a93b7128d2d928646b60d
Diksha217-ai <202401080032@mitaoe.ac.in>
2025-06-20 12:06:08 +0530

    Create DikshaAndVaishnavi.java

---
+++ DikshaAndVaishnavi.java
@@ -0,0 +1,15 @@
+public class DikshaAndVaishnavi {
+    public void greet(){
+        System.out.println("Hello, this is Diksha's first method speaking.");
+    }
+
+    public void greeting(){
+        System.out.println("Hi, this is Vaishnavi's second method saying hello.");
+    }
+
+    public static void main(String[] args){
+        DikshaAndVaishnavi obj1 = new DikshaAndVaishnavi();
+        obj1.greet();
+        obj1.greeting();
+    }
+}
